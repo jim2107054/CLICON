eff513946a383645dd7607df6225f44ecd0ed31b
jim2107054 <mdjahid205hasan@gmail.com>
2025-05-14 16:53:06 +0600

    .

--- frontend/src/components/navbars/ThirdNavbar.jsx
+++ frontend/src/components/navbars/ThirdNavbar.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { assets } from "../../assets/assets";
+import { NavLink } from "react-router-dom";
 
 const ThirdNavbar = () => {
   return (
     <div>
       <div className="flex items-center gap-5 py-2 px-36">
         <div className="bg-gray-200 border-1 px-2 py-1">
-          <select className="bg-gray-200 py-1 rounded" name="allCategory" id="allCategory">
+          <select
+            className="bg-gray-200 py-1 rounded"
+            name="allCategory"
+            id="allCategory"
+          >
             <option value="allCategory">All Category</option>
             <option value="computer">Computer</option>
             <option value="laptop">Laptop</option>
@@ -16,21 +21,41 @@ const ThirdNavbar = () => {
             <option value="Game">Game</option>
           </select>
         </div>
-        <div className="flex gap-1 text-base font-light items-center">
-          <img className="h-5" src={assets.location} alt="" />
-          <p>Track Order</p>
+        <div className="flex gap-1 text-base">
+          <NavLink
+            to="/track-order"
+            className="flex gap-1 text-base font-medium items-center thirdNavbar"
+          >
+            <img className="h-5" src={assets.location} alt="" />
+            <p>Track Order</p>
+          </NavLink>
         </div>
-        <div className="flex gap-1 text-base font-light items-center">
-          <img className="h-5" src={assets.compare} alt="" />
-          <p>Compare</p>
+        <div className="flex gap-1 text-base font-medium items-center">
+          <NavLink
+            to="/compare"
+            className="flex gap-1 text-base font-medium items-center thirdNavbar"
+          >
+            <img className="h-5" src={assets.compare} alt="" />
+            <p>Compare</p>
+          </NavLink>
         </div>
-        <div className="flex gap-1 text-base font-light items-center">
-          <img className="h-5" src={assets.headphone} alt="" />
-          <p>Customer Support</p>
+        <div className="flex gap-1 text-base font-medium items-center group">
+          <NavLink
+            to="/customer-support"
+            className="flex gap-1 text-base font-medium items-center thirdNavbar"
+          >
+            <img className="h-5" src={assets.headphone} alt="" />
+            <p>Customer Support</p>
+          </NavLink>
         </div>
-        <div className="flex gap-1 text-base font-light items-center">
-          <img className="h-5" src={assets.needHelp} alt="" />
-          <p>Need Help</p>
+        <div className="flex gap-1 text-base font-medium items-center">
+          <NavLink
+            to="/need-help"
+            className="flex gap-1 text-base font-medium items- thirdNavbar"
+          >
+            <img className="h-5" src={assets.needHelp} alt="" />
+            <p>Need Help</p>
+          </NavLink>
         </div>
       </div>
     </div>
