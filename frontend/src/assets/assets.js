34f362c6d54c6f881fcd029f0d4e469701151a5d
jim2107054 <mdjahid205hasan@gmail.com>
2025-05-14 13:14:12 +0600

    sales

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -28,8 +28,24 @@ import member5 from './CoreTeamMember/member5.png'
 import member6 from './CoreTeamMember/member6.png'
 import member7 from './CoreTeamMember/member7.png'
 import member8 from './CoreTeamMember/member8.png'
-
 import aboutBanner from './about-us/Banner.png'
+
+// import about us page photo(flash sales, best selling, etc)
+import flash1 from './about-us/flash-sale-today/Image1.png'
+import flash2 from './about-us/flash-sale-today/Image2.png'
+import flash3 from './about-us/flash-sale-today/Image3.png'
+
+import bestSeller1 from './about-us/best-sellers/Image1.png'
+import bestSeller2 from './about-us/best-sellers/Image2.png'
+import bestSeller3 from './about-us/best-sellers/Image3.png'
+
+import topRated1 from './about-us/top-rated/Image1.png'
+import topRated2 from './about-us/top-rated/Image2.png'
+import topRated3 from './about-us/top-rated/Image3.png'
+
+import newArrival1 from './about-us/new-arrival/Image1.png'
+import newArrival2 from './about-us/new-arrival/Image2.png'
+import newArrival3 from './about-us/new-arrival/Image3.png'
 
 
 export const assets = {
@@ -112,5 +128,89 @@ export const CoreTeamMembers = [
         name: 'Kevin Gilbert',
         position: 'UI Designer',
         image: member8,
+    }
+]
+
+export const flashSaleToday = [
+    {
+        id: 1,
+        image: flash1,
+        price: 1500,
+        description:"Bose Sport Earbuds - Wireless Earphones - Bluetooth In Ear..."
+    },
+    {
+        id: 2,
+        image: flash2,
+        price:1500,
+        description:"Simple Mobile 4G LTE Prepaid Smartphone"
+    },
+    {
+        id: 3,
+        image: flash3,
+        price: 1500,
+        description:"4K UHD LED Smart TV with Chromecast Built-in"
+    }
+]
+
+export const bestSellers = [
+    {
+        id: 1,
+        image: bestSeller1,
+        price: 1500,
+        description:"Samsung Electronics Samsung Galexy S21 5G"
+    },
+    {
+        id: 2,
+        image: bestSeller2,
+        price:1500,
+        description:"Simple Mobile 5G LTE Galexy 12 Mini 512GB Gaming Phone"
+    },
+    {
+        id: 3,
+        image: bestSeller3,
+        price: 1500,
+        description:"Sony DSCHX8 High Zoom Point & Shoot Camera"
+    }
+]
+
+export const topRated = [
+    {
+        id: 1,
+        image: topRated1,
+        price: 1500,
+        description:"Portable Wshing Machine, 11lbs capacity Model 18NMF..."
+    },
+    {
+        id: 2,
+        image: topRated2,
+        price:1500,
+        description:"Sony DSCHX8 High Zoom Point & Shoot Camera"
+    },
+    {
+        id: 3,
+        image: topRated3,
+        price: 1500,
+        description:"Dell Optiplex 7000x7480 All-in-One Computer Monitor"
+    }
+]
+
+export const newArrival = [
+    {
+        id: 1,
+        image: newArrival1,
+        price: 1500,
+        description:"TOZO T6 True Wireless Earbuds Bluetooth Headpho..."
+    },
+    {
+        id: 2,
+        image: newArrival2,
+        price:1500,
+        description:"JBL FLIP 4 - Waterproof Portable Bluetooth Speaker..."
+    },
+    {
+        id: 3,
+        image: newArrival3,
+        price: 1500,
+        description:"Wyze Cam Pan v2 1080p Pan/Tilt/Zoom Wi-Fi Indoor Smar..."
     }
 ]
