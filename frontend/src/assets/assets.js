7ae52c2d735b54d174208ad807460affa5b6a2fe
jim2107054 <mdjahid205hasan@gmail.com>
2025-05-14 12:34:19 +0600

    core team member info

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -19,6 +19,17 @@ import eye from './Eye.png'
 import checks from './Checks.png'
 import aboutOne from './aboutPage1.png'
 
+// import core team member photo
+import member1 from './member1.png'
+import member2 from './member2.png'
+import member3 from './member3.png'
+import member4 from './member4.png'
+import member5 from './member5.png'
+import member6 from './member6.png'
+import member7 from './member7.png'
+import member8 from './member8.png'
+
+
 export const assets = {
     fackbook,
     instagram,
@@ -39,5 +50,64 @@ export const assets = {
     needHelp,
     eye,
     checks,
-    aboutOne
+    aboutOne,
+    member1,
+    member2,
+    member3,
+    member4,
+    member5,
+    member6,
+    member7,
+    member8,
 }
+
+export const CoreTeamMembers = [
+    {
+        id: 1,
+        name: 'Kevin Gilbert',
+        position: 'Chief Executive Officer',
+        image: member1,
+    },
+    {
+        id: 2,
+        name: 'Kevin Gilbert',
+        position: 'Assistant of CEO',
+        image: member2,
+    },
+    {
+        id: 3,
+        name: 'Kevin Gilbert',
+        position: 'Head of Designer',
+        image: member3,
+    },
+    {
+        id: 4,
+        name: 'Kevin Gilbert',
+        position: 'UX Designer',
+        image: member4,
+    },
+    {
+        id: 5,
+        name: 'Kevin Gilbert',
+        position: 'Product Designer',
+        image: member5,
+    },
+    {
+        id: 6,
+        name: 'Kevin Gilbert',
+        position: 'Head of Development',
+        image: member6,
+    },
+    {
+        id: 7,
+        name: 'Kevin Gilbert',
+        position: 'Design Engineer',
+        image: member7,
+    },
+    {
+        id: 8,
+        name: 'Kevin Gilbert',
+        position: 'UI Designer',
+        image: member8,
+    }
+]
