66621b9d81c301d3676b12b7560afd50b4ee79cf
jim2107054 <mdjahid205hasan@gmail.com>
2025-05-23 23:07:21 +0600

    customer support

--- frontend/src/pages/CustomerSupport.jsx
+++ frontend/src/pages/CustomerSupport.jsx
@@ -5,7 +5,7 @@ import { FaArrowRight } from "react-icons/fa";
 const CustomerSupport = () => {
   return (
     <div className="">
-      <div className="flex flex-col gap-10 px-36 py-10">
+      <div className="flex flex-col gap-10 md:px-36 py-10">
         {/*------------First Div----------*/}
         <div className="w-full flex flex-col-reverse lg:flex-row gap-10">
           {/*---------Left side--------------*/}
@@ -46,7 +46,7 @@ const CustomerSupport = () => {
           <h1 className="text-2xl font-semibold text-center mt-5">
             What can we assist you with today?
           </h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-5">
+          <div className="grid px-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-5">
             <div className="customer-assist">
               <img className="" src={assets.truck} alt="" />
               <p>Track Order</p>
@@ -83,11 +83,11 @@ const CustomerSupport = () => {
         </div>
         {/*------------Third Div----------*/}
         <hr />
-        <div>
+        <div className="flex flex-col items-center justify-center px-5">
           <p className="text-2xl font-medium lg:text-center mt-5">Popular Topics</p>
-          <div className="flex flex-col justify-center lg:flex-row lg:gap-20 gap-8 mt-5">
+          <div className="flex px-5 sm:px-10 lg:px-0 flex-col justify-center lg:flex-row lg:gap-20 gap-8 mt-5">
             <div>
-              <ul className="gap-1 flex flex-col">
+              <ul className="gap-1 list-disc lg:list-none flex flex-col">
               <li>How do I return my item?</li>
               <li>What is Clicons Returns Policy?</li>
               <li>How long is the refund process?</li>
@@ -95,7 +95,7 @@ const CustomerSupport = () => {
             </div>
 
             <div>
-              <ul className="gap-1 flex flex-col">
+              <ul className="gap-1 list-disc lg:list-none flex flex-col">
               <li>What are the 'Delivery Timelines'?</li>
               <li>What is 'Discover Your Daraz Campaign 2022'?</li>
               <li>What is the Voucher & Gift Offer in this Campaign?</li>
@@ -103,7 +103,7 @@ const CustomerSupport = () => {
             </div>
 
             <div>
-              <ul className="gap-1 flex flex-col">
+              <ul className="gap-1 list-disc lg:list-none flex flex-col">
               <li>How to cancel Clicon Order.</li>
               <li>Ask the Digital and Device Community</li>
               <li>How to change my shop name?</li>
@@ -113,9 +113,9 @@ const CustomerSupport = () => {
         </div>
       </div>
       {/*------------Fourth Div----------*/}
-        <div className="bg-[#F2F4F5] px-44 py-10">
-          <div className="flex flex-col items-center">
-            <p className="bg-blue-500 w-fit px-3 py-1 rounded text-white items-center justify-center mb-5">CONTACT US</p>
+        <div className="bg-[#F2F4F5] px-5 md:px-44 py-10">
+          <div className="flex flex-col  items-center">
+            <p className="bg-blue-500  md:w-fit px-3 py-1 rounded text-white items-center justify-center mb-5">CONTACT US</p>
             <h1 className="text-3xl font-medium">Don't find your answer.</h1>
             <p className="text-3xl font-medium">Contact with us</p>
           </div>
